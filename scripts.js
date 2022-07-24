2dea50075ec918856adc2cfc5051231ce777f8ba
Wesley Araújo <wesle202@gmail.com>
2022-07-23 23:03:34 -0300

    fix: corrected countdown

--- scripts.js
+++ scripts.js
@@ -1,15 +1,43 @@
 
+function timeFormat(num){
+    if(num<10){
+        return  `0${num}` ;
+    }else{
+        return `${num}`;
+    }
+}
+
+
+// Pegando um horário para começar a contar regressivamente 
+var count = new Date();
+var day = count.getDate();
+var hours = count.getHours();
+var minutes = count.getMinutes();
+var seconds=  count.getSeconds();
+
+
+
+
 const interval = setInterval(() => {
 
-    const count = new Date()
 
-    const day = count.getDate()+ ':' 
-    const hours = count.getHours() + ':' 
-    const minutes = count.getMinutes() + ':'
-    const seconds=  count.getSeconds();
+     if(seconds == 0){
+         minutes--;
+         seconds = 59;
+     }else if(minutes ==0){
+         hours--;
+         minutes = 59;
+     }else if(hours == 0){
+         days--;
+         hours = 23;
+     }else{
+         seconds--;
+     }
 
-    document.getElementById('day').innerHTML = day;
-    document.getElementById('hours').innerHTML = hours;
-    document.getElementById('minutes').innerHTML = minutes;
-    document.getElementById('seconds').innerHTML = seconds;
+     
+    document.getElementById('day').innerHTML = `${timeFormat(day)}:`;
+    document.getElementById('hours').innerHTML = `${timeFormat(hours)}:`;
+    document.getElementById('minutes').innerHTML = `${timeFormat(minutes)}:`;
+    document.getElementById('seconds').innerHTML = `${timeFormat(seconds)}`;
+
 }, 1000);
